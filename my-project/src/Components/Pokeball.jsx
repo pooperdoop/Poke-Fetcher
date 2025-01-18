165235974ee5c987c3011cca542a2989cac446bf
Bustah <psnatb2lme@gmail.com>
2025-01-18 22:28:34 +0800

    minor tweaks

--- my-project/src/Components/Pokeball.jsx
+++ my-project/src/Components/Pokeball.jsx
@@ -8,22 +8,22 @@ export default function Pokeball(){
 
 
     const [isVisible, setIsVisible] = useState(true);
-    const [pokemon, setPokemon] = useState('');
+    const [pokemon, setPokemon] = useState("");
 
     const transition = useTransition(isVisible, {
-        from: {x:0, y:-800, opacity: 0},
-        enter: {x:0, y:0, opacity: 1},
-        leave: {x:0, y:-800, opacity: 0},
+        from: {x:0, y:-700},
+        enter: {x:0, y:0},
+        leave: {x:0, y:-700},
     });
 
     const transition2 = useTransition(isVisible, {
-        from: {x:0, y:800, opacity: 0},
-        enter: {x:0, y:0, opacity: 1},
-        leave: {x:0, y:800, opacity: 0},
+        from: {x:0, y:700},
+        enter: {x:0, y:0},
+        leave: {x:0, y:700},
     });
 
     function handleIsVisible(){
-        if(pokemon.trim !== ''){
+        if(pokemon.trim() !== ""){
             setIsVisible(false);
         }
     }
@@ -32,7 +32,7 @@ export default function Pokeball(){
                {transition((style, item) => item ? <animated.header style={style} className="h-2/4 bg-red-500 w-full flex justify-center items-center relative">
 
                     <h1 className='siteTitle absolute top-10 text-yellow-300'>Poké-Fetcher</h1>
-                    <input className='rounded-lg w-1/6 min-w-52 p-3 border-4 border-black mt-10 outline-none' type="text" placeholder='Enter Pokémon Name...' value={pokemon} onChange={p => setPokemon(event.target.value)}/>
+                    <input className='rounded-lg w-1/6 min-w-52 p-3 border-4 border-black mt-10 outline-none' type="text" placeholder='Enter Pokémon Name...' value={pokemon} onChange={() => setPokemon(event.target.value)}/>
 
                     <div className="h-1/6 bg-black w-full -bottom-10 absolute flex justify-center items-center">
                         
