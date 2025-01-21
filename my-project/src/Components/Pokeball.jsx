7194135ef160badbc52bad2be9aa768da147fa2a
Bustah <psnatb2lme@gmail.com>
2025-01-21 12:43:07 +0800

    pokemon info fetching function created

--- my-project/src/Components/Pokeball.jsx
+++ my-project/src/Components/Pokeball.jsx
@@ -6,9 +6,11 @@ import innerCircle from '../assets/inner-circle.svg';
 
 export default function Pokeball(){
 
-    let i = 0;
 
     const [isVisible, setIsVisible] = useState(true);
+    const [pokemonSprite, setPokemonSprite] = useState("");
+    const [pokemonID, setPokemonID] = useState("");
+    const [pokemonName, setPokemonName] = useState("");
     const [pokemon, setPokemon] = useState("");
 
     const transition = useTransition(isVisible, {
@@ -26,29 +28,54 @@ export default function Pokeball(){
     const transition3 = useTransition(isVisible, {
     });
 
+    // for animation of each innerDiv
     const AnimatedBlock = ({ className, animationConfig, text, children }) => {
         const props = useSpring(animationConfig);
-
-        console.log(i++)
         
         return (
           <animated.div className={className} style={props}>
-            {text}
+            {text} {children}
           </animated.div>
         );
       };
 
-    function handleIsVisible(){
+    async function handleIsVisible(){
         if(pokemon.trim() !== ""){
-            setIsVisible(c => !c);
+            try{
+                // searching the API if pokemon exists
+                const fetchedPokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.toLowerCase()}`);
+
+                if(!fetchedPokemon.ok){
+                    alert("There is no such Pokemon!");
+                    return;
+                } else{
+                    //getting pokemon data from API
+                    const pokemonData = await fetchedPokemon.json();
+                    const pokemonSprite = pokemonData.sprites.front_default;
+                    const pokemonID = pokemonData.id;
+                    const pokemonName = pokemonData.name;
+                    console.log(pokemonData);
+                    setPokemonSprite(s => s = pokemonSprite);
+                    setPokemonID(I => I = pokemonID);
+                    setPokemonName(n => n = pokemonName);
+
+
+                    setIsVisible(c => !c);
+             }
+            } catch(e){
+
+            };
+
         }
     }
     
     return(<section className='h-full'> 
 
+                <header className='absolute -z-10 bg-blue-400 h-16 w-full '>
 
+                </header>
 
-               {transition((style, item) => item ? <animated.header style={style} className="h-2/4 bg-red-500 w-full flex justify-center items-center relative">
+               {transition((style, item) => item ? <animated.div style={style} className="h-2/4 bg-red-500 w-full flex justify-center items-center relative">
                     <h1 className='siteTitle absolute top-10 text-yellow-300'>Poké-Fetcher</h1>
                     <input className='rounded-lg w-1/6 min-w-52 p-3 border-4 border-black mt-10 outline-none' type="text" placeholder='Enter Pokémon Name...' value={pokemon} onChange={() => setPokemon(event.target.value)}/>
 
@@ -58,29 +85,34 @@ export default function Pokeball(){
                         <h1 onClick={handleIsVisible} className='absolute z-10 cursor-pointer transition-all hover:text-red-200' id='searchText'>Search</h1>
                         <img onClick={handleIsVisible} className='w-1/12 min-w-32 absolute cursor-pointer' id='innerCircle' src={innerCircle} alt="search" />
                     </div>
-                    </animated.header> : '' )}
+                    </animated.div> : '' )}
               
                 {transition2((style, item) => item ?
-                    <animated.footer style={style} className='h-2/4 flex justify-center bg-white relative -z-10'>
+                    <animated.div style={style} className='h-2/4 flex justify-center bg-white relative -z-10'>
                         <h1 className='text-black z-10 absolute bottom-0'>&copy; 2025 by Pooperdoop</h1>
-                    </animated.footer> : '')}
+                    </animated.div> : '')}
 
-                {transition3((style, item) => item ? "":                
-                    <div style={style} className='h-full w-full bg-gray-200 absolute -z-10 flex justify-center items-center grid grid-cols-8 overflow-hidden'>
+                {transition3((style, item) => item ? "":        
+
+                    <div style={style} className='h-full w-full bg-blue-200 absolute -z-20 flex justify-center items-center grid grid-cols-8 overflow-hidden'>
 
                 
-                        <div className=' bg-gray-200 col-span-full xl:col-start-2 xl:col-end-8 h-3/4 rounded-lg grid grid-cols-8 grid-rows-4 md:grid-flow-row sm:grid-flow-row'>
+                        <div className=' bg-blue-200 col-span-full xl:col-start-2 xl:col-end-8 h-3/4 rounded-lg grid grid-cols-8 grid-rows-4 md:grid-flow-row sm:grid-flow-row'>
 
                         <AnimatedBlock className='bg-red-300 row-span-2 lg:col-span-2 sm:col-span-3 col-span-full infoDiv divBorder' 
-                            animationConfig={{ from: { transform: 'translateX(-100vw)' }, to: { transform: 'translateX(0)',  config:{duration:200}}}} text="01" />
+                            animationConfig={{ from: { transform: 'translateX(-100vw)' }, to: { transform: 'translateX(0)',  config:{duration:200}}}}>
+                                <img className='w-full h-full pokemonSprite' src={pokemonSprite} />
+                                </AnimatedBlock> 
 
                         <AnimatedBlock className='bg-blue-300 row-span-1 lg:col-span-6 sm:col-span-5 col-span-full infoDiv divBorder' 
-                            animationConfig={{ from: { transform: 'translateX(100vw)' }, to: { transform: 'translateX(0)' },  config:{duration:300}}} text="02" />
+                            animationConfig={{ from: { transform: 'translateX(100vw)' }, to: { transform: 'translateX(0)' },  config:{duration:300}}} >
+                                <h1 className='z-10'>#{pokemonID}: {pokemonName}</h1>
+                            </AnimatedBlock>
 
                         <AnimatedBlock className='bg-yellow-300 row-span-1 lg:col-span-6 sm:col-span-5 col-span-full infoDiv divBorder' 
                             animationConfig={{ from: {  transform: 'translateX(100vw)' }, to: {  transform: 'translateX(0)' },  config:{duration:600}}} text="03" />
 
-                        <div className=' bg-gray-200 row-span-2 sm:col-span-3 col-span-full grid grid-cols-4 grid-rows-4 m-7 infoDiv4'>
+                        <div className=' bg-blue-200  row-span-2 sm:col-span-3 col-span-full grid grid-cols-4 grid-rows-4 m-7 infoDiv4'>
                             <AnimatedBlock className="innerInfoDiv divBorder bg-green-100 col-span-2 row-span-2" 
                                 animationConfig={{ from: { transform: 'translateX(-100vw)'}, to: { transform: 'translateX(0)'},  config:{duration:600} }} text="4.1" />
 
@@ -98,6 +130,6 @@ export default function Pokeball(){
                             animationConfig={{ from: { transform: 'translateY(100vh)' }, to: { transform: 'translateY(0)' },  config:{duration:700}}} text="05" /> 
 
                         </div> 
-                    </div>)}
+                    </div> )}
             </section>);
 }
